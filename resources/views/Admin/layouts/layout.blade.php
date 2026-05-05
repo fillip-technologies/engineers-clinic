90323b9e62a37f51a0084a79872f8d29ee626ca0
Shruti-Orbit <scdhr21@gmail.com>
2026-05-05 15:45:24 +0530

    tsrt

--- resources/views/Admin/layouts/layout.blade.php
+++ resources/views/Admin/layouts/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ $title ?? config('app.name', 'Engineers Clini') }}</title>
+    <title>{{ $title ?? config('app.name', 'Engineers Clinic') }}</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
